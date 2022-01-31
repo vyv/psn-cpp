30753d2c34fab1679660b6051810233dbf3dfc3e
Stephen Buchanan <stephen.buchanan@random.studio>
2022-01-31 17:21:31 +0100

    working sending and receiving UDP (not PSN)

--- examples/cmake/psn_client.cpp
+++ examples/cmake/psn_client.cpp
@@ -25,7 +25,7 @@
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 #include <psn_lib.hpp>
-#include "UdpServerSocket.hpp"
+#include <kissnet.hpp>
 
 #include <iostream>
 
@@ -34,9 +34,11 @@
 #include <chrono>
 #include <thread>
 
-const short           PORT         = 8888;
+const uint16_t           PORT         = 56565;
 static const short    BUFLEN       = 1024;
 static const uint32_t TIMEOUT_MSEC = 1000;
+
+namespace kn = kissnet;
 
 int main( void )
 {
@@ -46,8 +48,11 @@ int main( void )
 
 //====================================================
 // Init client
+    //Socket used to receive, the "endpoint" is where to listen to data
+    kn::udp_socket b_socket(kn::endpoint("0.0.0.0", 56565));
+    b_socket.bind();
 
-    UdpServerSocket server(PORT, TIMEOUT_MSEC);
+    // UdpServerSocket server(PORT, TIMEOUT_MSEC);
     // udp_socket socket_client ;
     // socket_client.bind( ::psn::DEFAULT_UDP_PORT ) ;
     // socket_client.join_multicast_group( ::psn::DEFAULT_UDP_MULTICAST_ADDR ) ;
@@ -69,82 +74,101 @@ int main( void )
         printf("Waiting for data...");
         fflush(stdout);
 
-        memset(buf, 0, BUFLEN);
+        // memset(buf, 0, BUFLEN);
 
-        *buf = 0;
+        // *buf = 0;
 
-        server.receiveData(buf, BUFLEN);
+        // server.receiveData(buf, BUFLEN);
+        kn::buffer<1024> recv_buff;
 
-        if (*buf) {
-            printf("Data: %s\n", buf);
-            // server.sendData(buf, strlen(buf));
+        //Actually print bytes_available
+		std::cout << "avaliable in UDP socket : " << b_socket.bytes_available() << " bytes\n";
 
-            // std::cout << "As string: " << msg << " with size " << msg.size() << std::endl;
+		//You receive in the same way
+		auto [received_bytes, status] = b_socket.recv(recv_buff);
+		const auto from = b_socket.get_recv_endpoint();
+
+		//Print the data
+		std::cout << "Received: ";
+
+		for(unsigned char i = 0; i < 16; i++)
+		{
+			std::cout << std::hex << std::to_integer<int>(recv_buff[i]) << std::dec << ' ';
+		}
+
+		//Print who send the data
+		std::cout << "From: " << from.address << ' ' << from.port << '\n';
+
+        // if (recv_buff) {
+        //     printf("Data: %s\n", buf);
+        //     // server.sendData(buf, strlen(buf));
+
+        //     // std::cout << "As string: " << msg << " with size " << msg.size() << std::endl;
             
-            psn_decoder.decode( buf , BUFLEN ) ;
+        //     psn_decoder.decode( buf , BUFLEN ) ;
 
-            // if ( psn_decoder.get_data().header.frame_id != last_frame_id )
-            // {
-                last_frame_id = psn_decoder.get_data().header.frame_id ;
+        //     // if ( psn_decoder.get_data().header.frame_id != last_frame_id )
+        //     // {
+        //         last_frame_id = psn_decoder.get_data().header.frame_id ;
 
-                const ::psn::tracker_map & recv_trackers = psn_decoder.get_data().trackers ;
+        //         const ::psn::tracker_map & recv_trackers = psn_decoder.get_data().trackers ;
                 
-                // if ( skip_cout++ % 20 == 0 )
-                // {
-                    ::std::cout << "--------------------PSN CLIENT-----------------" << ::std::endl ;
-                    ::std::cout << "System Name: " << psn_decoder.get_info().system_name << ::std::endl ;
-                    ::std::cout << "Frame Id: " << (int)last_frame_id << ::std::endl ;
-                    ::std::cout << "Frame Timestamp: " << psn_decoder.get_data().header.timestamp_usec << ::std::endl ;
-                    ::std::cout << "Tracker count: " << recv_trackers.size() << ::std::endl ;
+        //         // if ( skip_cout++ % 20 == 0 )
+        //         // {
+        //             ::std::cout << "--------------------PSN CLIENT-----------------" << ::std::endl ;
+        //             ::std::cout << "System Name: " << psn_decoder.get_info().system_name << ::std::endl ;
+        //             ::std::cout << "Frame Id: " << (int)last_frame_id << ::std::endl ;
+        //             ::std::cout << "Frame Timestamp: " << psn_decoder.get_data().header.timestamp_usec << ::std::endl ;
+        //             ::std::cout << "Tracker count: " << recv_trackers.size() << ::std::endl ;
 
-                    for ( auto it = recv_trackers.begin() ; it != recv_trackers.end() ; ++it )
-                    {
-                        const ::psn::tracker & tracker = it->second ;
+        //             for ( auto it = recv_trackers.begin() ; it != recv_trackers.end() ; ++it )
+        //             {
+        //                 const ::psn::tracker & tracker = it->second ;
 
-                        ::std::cout << "Tracker - id: " << tracker.get_id() << " | name: " << tracker.get_name() << ::std::endl ;
+        //                 ::std::cout << "Tracker - id: " << tracker.get_id() << " | name: " << tracker.get_name() << ::std::endl ;
 
-                        if ( tracker.is_pos_set() )
-                            ::std::cout << "    pos: " << tracker.get_pos().x << ", " << 
-                                                          tracker.get_pos().y << ", " <<
-                                                          tracker.get_pos().z << std::endl ;
+        //                 if ( tracker.is_pos_set() )
+        //                     ::std::cout << "    pos: " << tracker.get_pos().x << ", " << 
+        //                                                   tracker.get_pos().y << ", " <<
+        //                                                   tracker.get_pos().z << std::endl ;
 
-                        if ( tracker.is_speed_set() )
-                            ::std::cout << "    speed: " << tracker.get_speed().x << ", " << 
-                                                            tracker.get_speed().y << ", " <<
-                                                            tracker.get_speed().z << std::endl ;
+        //                 if ( tracker.is_speed_set() )
+        //                     ::std::cout << "    speed: " << tracker.get_speed().x << ", " << 
+        //                                                     tracker.get_speed().y << ", " <<
+        //                                                     tracker.get_speed().z << std::endl ;
 
-                        if ( tracker.is_ori_set() )
-                            ::std::cout << "    ori: " << tracker.get_ori().x << ", " << 
-                                                          tracker.get_ori().y << ", " <<
-                                                          tracker.get_ori().z << std::endl ;
+        //                 if ( tracker.is_ori_set() )
+        //                     ::std::cout << "    ori: " << tracker.get_ori().x << ", " << 
+        //                                                   tracker.get_ori().y << ", " <<
+        //                                                   tracker.get_ori().z << std::endl ;
 
-                        if ( tracker.is_status_set() )
-                            ::std::cout << "    status: " << tracker.get_status() << std::endl ;
+        //                 if ( tracker.is_status_set() )
+        //                     ::std::cout << "    status: " << tracker.get_status() << std::endl ;
 
-                        if ( tracker.is_accel_set() )
-                            ::std::cout << "    accel: " << tracker.get_accel().x << ", " << 
-                                                            tracker.get_accel().y << ", " <<
-                                                            tracker.get_accel().z << std::endl ;
+        //                 if ( tracker.is_accel_set() )
+        //                     ::std::cout << "    accel: " << tracker.get_accel().x << ", " << 
+        //                                                     tracker.get_accel().y << ", " <<
+        //                                                     tracker.get_accel().z << std::endl ;
 
-                        if ( tracker.is_target_pos_set() )
-                            ::std::cout << "    target pos: " << tracker.get_target_pos().x << ", " << 
-                                                                 tracker.get_target_pos().y << ", " <<
-                                                                 tracker.get_target_pos().z << std::endl ;
+        //                 if ( tracker.is_target_pos_set() )
+        //                     ::std::cout << "    target pos: " << tracker.get_target_pos().x << ", " << 
+        //                                                          tracker.get_target_pos().y << ", " <<
+        //                                                          tracker.get_target_pos().z << std::endl ;
 
-                        if ( tracker.is_timestamp_set() )
-                            ::std::cout << "    timestamp: " << tracker.get_timestamp() << std::endl ;
-                    }
+        //                 if ( tracker.is_timestamp_set() )
+        //                     ::std::cout << "    timestamp: " << tracker.get_timestamp() << std::endl ;
+        //             }
 
-                    ::std::cout << "-----------------------------------------------" << ::std::endl ;
-                //} // skip
+        //             ::std::cout << "-----------------------------------------------" << ::std::endl ;
+        //         //} // skip
 
-        }
+        // }
 
-        else {
-            printf("\n");
-            // sleep(1);
-            std::this_thread::sleep_for(std::chrono::milliseconds(1) );
-        }
+        // else {
+        //     printf("\n");
+        //     // sleep(1);
+        //     std::this_thread::sleep_for(std::chrono::milliseconds(1) );
+        // }
 
 
 
@@ -153,6 +177,8 @@ int main( void )
 
         
         // }
+        std::this_thread::sleep_for(std::chrono::milliseconds(1) );
+
     }
 
     return 0;
--- examples/cmake/psn_server.cpp
+++ examples/cmake/psn_server.cpp
@@ -25,7 +25,7 @@
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 #include "psn_lib.hpp"
-#include "UdpClientSocket.hpp"
+#include <kissnet.hpp>
 
 #include <cmath>
 #include <iostream>
@@ -36,7 +36,9 @@
 #include <thread>
 
 const char * HOST = "127.0.0.1";
-const short  PORT = 8888;
+const uint16_t  PORT = 8888;
+
+namespace kn = kissnet;
 
 int main( void )
 {
@@ -47,118 +49,133 @@ int main( void )
     // udp_socket socket_server ;
     // socket_server.enable_send_message_multicast() ;
 
-    UdpClientSocket client(HOST, PORT);
-
-    ::psn::psn_encoder psn_encoder( "Test PSN Server" ) ;
-
-    ::psn::tracker_map trackers ;
-    int i = 0 ;
-    trackers[ i ] = ::psn::tracker( i++ , "Sun" ) ;
-    trackers[ i ] = ::psn::tracker( i++ , "Mercury" ) ;
-    trackers[ i ] = ::psn::tracker( i++ , "Venus" ) ;
-    trackers[ i ] = ::psn::tracker( i++ , "Earth" ) ;
-    trackers[ i ] = ::psn::tracker( i++ , "Mars" ) ;
-    trackers[ i ] = ::psn::tracker( i++ , "Jupiter" ) ;
-    trackers[ i ] = ::psn::tracker( i++ , "Saturn" ) ;
-    trackers[ i ] = ::psn::tracker( i++ , "Uranus" ) ;
-    trackers[ i ] = ::psn::tracker( i++ , "Neptune" ) ;
-    trackers[ i ] = ::psn::tracker( i++ , "Pluto" ) ;
-
-    // Planets orbit in days
-    float orbits[ 10 ] = { 1.0f , 88.0f , 224.7f , 365.2f , 687.0f , 4332.0f , 10760.0f , 30700.0f , 60200.0f , 90600.0f } ;
-    // Planets distance from sun in million km x 100
-    float dist_from_sun[ 10 ] = { 0.0f , 0.58f , 1.08f , 1.50f , 2.28f , 7.78f , 14.29f , 28.71f , 45.04f , 59.13f } ;
+    // UdpClientSocket client(HOST, PORT);
     
-    uint64_t timestamp = 0 ;
+    //Socket used to send, the "endpoint" is the destination of the data
+    kn::udp_socket a_socket(kn::endpoint("127.0.0.1", 56565));
 
-    //====================================================
-    // Main loop
-    while ( true ) 
-    {
-        // Update trackers
-        for ( int i = 1 ; i < 10 ; ++i ) // do not update the sun
-        {
-            float a = 1.0f / orbits[ i ] ;
-            float b = dist_from_sun[ i ] ;
-            float x = (float)timestamp ;
-            float cb = ::std::cosf( a * x ) * b ;
-            float sb = ::std::sinf( a * x ) * b ;
-            trackers[ i ].set_pos( ::psn::float3( sb , 0 , cb ) ) ;
-            trackers[ i ].set_speed( ::psn::float3( a * cb , 0 , -a * sb )  ) ;
-            trackers[ i ].set_ori( ::psn::float3( 0 , x / 1000.0f , 0 ) ) ;
-            trackers[ i ].set_accel( ::psn::float3( ::psn::float3( -a * a * sb , 0 , -a * a * cb )  ) ) ;
-            trackers[ i ].set_target_pos( ::psn::float3( 3 , 14 , 16 ) ) ;
-            trackers[ i ].set_status( i / 10.0f ) ;
-            trackers[ i ].set_timestamp( timestamp ) ;
-        }
+    //Byte buffer
+    kn::buffer<16> buff;
 
-        // Send data
-        if ( timestamp % 16 == 0 ) // transmit data at 60 Hz approx.
-        {
-            std::cout << "timestamp = " << timestamp << std::endl;
+    //Build data to send (flat array of bytes
+    for(unsigned char i = 0; i < 16; i++)
+        buff[i] = std::byte{ i };
 
-            ::std::list< ::std::string > data_packets = psn_encoder.encode_data( trackers , timestamp ) ;
+    //Send data
+    a_socket.send(buff.data(), 16);
 
-            ::std::cout << "--------------------PSN SERVER-----------------" << ::std::endl ;
-            ::std::cout << "Sending PSN_DATA_PACKET : " 
-                        << "Frame Id = " << (int)psn_encoder.get_last_data_frame_id() << std::endl
-                        << "Tracker count = " << (int)trackers.size()
-                        << " , Packet Count = " << data_packets.size() << ::std::endl ;
+    std::cout << "OK" << std::endl;
 
-            for ( auto it = data_packets.begin() ; it != data_packets.end() ; ++it )
-            {
-                // Uncomment these lines if you want to simulate a packet drop now and then
-                /*static uint64_t packet_drop = 0 ;
-                if ( packet_drop++ % 100 != 0 )*/
+    // ::psn::psn_encoder psn_encoder( "Test PSN Server" ) ;
+
+    // ::psn::tracker_map trackers ;
+    // int i = 0 ;
+    // trackers[ i ] = ::psn::tracker( i++ , "Sun" ) ;
+    // trackers[ i ] = ::psn::tracker( i++ , "Mercury" ) ;
+    // trackers[ i ] = ::psn::tracker( i++ , "Venus" ) ;
+    // trackers[ i ] = ::psn::tracker( i++ , "Earth" ) ;
+    // trackers[ i ] = ::psn::tracker( i++ , "Mars" ) ;
+    // trackers[ i ] = ::psn::tracker( i++ , "Jupiter" ) ;
+    // trackers[ i ] = ::psn::tracker( i++ , "Saturn" ) ;
+    // trackers[ i ] = ::psn::tracker( i++ , "Uranus" ) ;
+    // trackers[ i ] = ::psn::tracker( i++ , "Neptune" ) ;
+    // trackers[ i ] = ::psn::tracker( i++ , "Pluto" ) ;
+
+    // // Planets orbit in days
+    // float orbits[ 10 ] = { 1.0f , 88.0f , 224.7f , 365.2f , 687.0f , 4332.0f , 10760.0f , 30700.0f , 60200.0f , 90600.0f } ;
+    // // Planets distance from sun in million km x 100
+    // float dist_from_sun[ 10 ] = { 0.0f , 0.58f , 1.08f , 1.50f , 2.28f , 7.78f , 14.29f , 28.71f , 45.04f , 59.13f } ;
+    
+    // uint64_t timestamp = 0 ;
+
+    // //====================================================
+    // // Main loop
+    // while ( true ) 
+    // {
+    //     // Update trackers
+    //     for ( int i = 1 ; i < 10 ; ++i ) // do not update the sun
+    //     {
+    //         float a = 1.0f / orbits[ i ] ;
+    //         float b = dist_from_sun[ i ] ;
+    //         float x = (float)timestamp ;
+    //         float cb = ::std::cosf( a * x ) * b ;
+    //         float sb = ::std::sinf( a * x ) * b ;
+    //         trackers[ i ].set_pos( ::psn::float3( sb , 0 , cb ) ) ;
+    //         trackers[ i ].set_speed( ::psn::float3( a * cb , 0 , -a * sb )  ) ;
+    //         trackers[ i ].set_ori( ::psn::float3( 0 , x / 1000.0f , 0 ) ) ;
+    //         trackers[ i ].set_accel( ::psn::float3( ::psn::float3( -a * a * sb , 0 , -a * a * cb )  ) ) ;
+    //         trackers[ i ].set_target_pos( ::psn::float3( 3 , 14 , 16 ) ) ;
+    //         trackers[ i ].set_status( i / 10.0f ) ;
+    //         trackers[ i ].set_timestamp( timestamp ) ;
+    //     }
+
+    //     // Send data
+    //     if ( timestamp % 16 == 0 ) // transmit data at 60 Hz approx.
+    //     {
+    //         std::cout << "timestamp = " << timestamp << std::endl;
+
+    //         ::std::list< ::std::string > data_packets = psn_encoder.encode_data( trackers , timestamp ) ;
+
+    //         ::std::cout << "--------------------PSN SERVER-----------------" << ::std::endl ;
+    //         ::std::cout << "Sending PSN_DATA_PACKET : " 
+    //                     << "Frame Id = " << (int)psn_encoder.get_last_data_frame_id() << std::endl
+    //                     << "Tracker count = " << (int)trackers.size()
+    //                     << " , Packet Count = " << data_packets.size() << ::std::endl ;
+
+    //         for ( auto it = data_packets.begin() ; it != data_packets.end() ; ++it )
+    //         {
+    //             // Uncomment these lines if you want to simulate a packet drop now and then
+    //             /*static uint64_t packet_drop = 0 ;
+    //             if ( packet_drop++ % 100 != 0 )*/
 
                 
-                    // socket_server.send_message( ::psn::DEFAULT_UDP_MULTICAST_ADDR , ::psn::DEFAULT_UDP_PORT , *it ) ;
+    //                 // socket_server.send_message( ::psn::DEFAULT_UDP_MULTICAST_ADDR , ::psn::DEFAULT_UDP_PORT , *it ) ;
 
-                std::cout << "Send:" << it->c_str() << " of length " << it->length() << std::endl;
+    //             std::cout << "Send:" << it->c_str() << " of length " << it->length() << std::endl;
 
-                // void* buf;
-                // std::string s (it->c_str());
-                // std::cout << "Converted? " << s << std::endl;
-                // buf = static_cast<void *>(&s);
-                // client.sendData(buf, it->length());
-
-
-                char buf[it->length()];
-                for (int i = 0; i < it->length(); i++) {
-                  // std::cout << "#" << i << " : " << (uint)it->c_str()[i] << std::endl;
-                  buf[i] = it->c_str()[i];
-                }
+    //             // void* buf;
+    //             // std::string s (it->c_str());
+    //             // std::cout << "Converted? " << s << std::endl;
+    //             // buf = static_cast<void *>(&s);
+    //             // client.sendData(buf, it->length());
 
 
-                client.sendData(buf, it->length());
+    //             char buf[it->length()];
+    //             for (int i = 0; i < it->length(); i++) {
+    //               // std::cout << "#" << i << " : " << (uint)it->c_str()[i] << std::endl;
+    //               buf[i] = it->c_str()[i];
+    //             }
 
-            }
 
-            ::std::cout << "-----------------------------------------------" << ::std::endl ;
-        }
+    //             client.sendData(buf, it->length());
 
-        // Send Info
-        if ( timestamp % 1000 == 0 ) // transmit info at 1 Hz approx.
-        {
-            ::std::list< ::std::string > info_packets = psn_encoder.encode_info( trackers , timestamp ) ;
+    //         }
 
-            ::std::cout << "--------------------PSN SERVER-----------------" << ::std::endl ;
-            ::std::cout << "Sending PSN_INFO_PACKET : " 
-                        << "Frame Id = " << (int)psn_encoder.get_last_info_frame_id()
-                        << " , Packet Count = " << info_packets.size() << ::std::endl ;
+    //         ::std::cout << "-----------------------------------------------" << ::std::endl ;
+    //     }
+
+    //     // Send Info
+    //     if ( timestamp % 1000 == 0 ) // transmit info at 1 Hz approx.
+    //     {
+    //         ::std::list< ::std::string > info_packets = psn_encoder.encode_info( trackers , timestamp ) ;
+
+    //         ::std::cout << "--------------------PSN SERVER-----------------" << ::std::endl ;
+    //         ::std::cout << "Sending PSN_INFO_PACKET : " 
+    //                     << "Frame Id = " << (int)psn_encoder.get_last_info_frame_id()
+    //                     << " , Packet Count = " << info_packets.size() << ::std::endl ;
    
-            for ( auto it = info_packets.begin() ; it != info_packets.end() ; ++it )
-              // TODO: send info packet using client.sendData
-                // socket_server.send_message( ::psn::DEFAULT_UDP_MULTICAST_ADDR , ::psn::DEFAULT_UDP_PORT , *it ) ;
+    //         for ( auto it = info_packets.begin() ; it != info_packets.end() ; ++it )
+    //           // TODO: send info packet using client.sendData
+    //             // socket_server.send_message( ::psn::DEFAULT_UDP_MULTICAST_ADDR , ::psn::DEFAULT_UDP_PORT , *it ) ;
 
 
-            ::std::cout << "-----------------------------------------------" << ::std::endl ;
-        }
+    //         ::std::cout << "-----------------------------------------------" << ::std::endl ;
+    //     }
 
-        // sleep( 1  ) ;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1) );
-        timestamp++ ;    
-    }
+    //     // sleep( 1  ) ;
+    //     std::this_thread::sleep_for(std::chrono::milliseconds(1) );
+    //     timestamp++ ;    
+    // }
 
     return 0;
 }
